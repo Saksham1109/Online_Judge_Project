57a5d62e91d7d41de477e1ee23f400cd3ce685d9
saksham <sakshammishra.mishra403@gmail.com>
2023-09-08 20:14:10 +0530

    Executing CPP codebase

---
+++ backEnd/executeCpp.js
@@ -0,0 +1,39 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const outputPath = path.join(__dirname, "outputs");
+
+if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true });
+}
+
+const executeCpp = (filepath) => {
+    const jobId = path.basename(filepath).split(".")[0];
+    console.log("Here is the jobId");
+    console.log(jobId);
+    console.log("==================================");
+    const outPath = path.join(outputPath, `${jobId}.exe`);
+    console.log(outPath);
+
+    console.log(`g++ ${filepath} -o ${outPath} ; cd ${outputPath} ; .\\${jobId}.exe`);
+
+    return new Promise((resolve, reject) => {
+        exec(
+            `g++ ${filepath} -o ${outPath} && cd ${outputPath} && .\\${jobId}.exe`,
+            (error, stdout, stderr) => {
+                if (error) {
+                    reject({ error, stderr });
+                }
+                if (stderr) {
+                    reject(stderr);
+                }
+                resolve(stdout);
+            }
+        );
+    });
+};
+
+module.exports = {
+    executeCpp,
+};
