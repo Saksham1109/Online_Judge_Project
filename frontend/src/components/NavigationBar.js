dc0b1f2bd9ab6cf2b27227d63fadfbf74e97b8e8
Saksham Mishra <sakshammishra.mishra403@gmail.com>
2023-09-28 18:26:34 +0530

    Commit to reset

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import HomeScreen from '../screens/HomeScreen'
-import { Button, Navbar,Nav,NavItem,NavDropdown } from 'react-bootstrap'
+import { Button, Navbar,Nav,NavDropdown } from 'react-bootstrap'
 function NavigationBar() {
   return (
     <div>
@@ -12,7 +11,7 @@ function NavigationBar() {
         <Nav className="me-auto">  
           <Nav.Link as={Link} to="/">Home</Nav.Link>  
           {/* <Nav.Link as={Link} to="/viewproblems">Problems</Nav.Link>   */}
-          <Nav.Link as={Link} to ="/signin">Sign-In</Nav.Link>  
+          <Nav.Link as={Link} to ="/signin">Register/Sign-In</Nav.Link>  
           <NavDropdown title="UserProfile" id="basic-nav-dropdown">  
             <NavDropdown.Item as={Link} to ="/viewProfile">View Profile</NavDropdown.Item>  
             <NavDropdown.Item as={Link} to ="/logout">Logout</NavDropdown.Item>  
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ListOfOProblems from '../components/ListOfOProblems'
+import NavigationBar from '../components/NavigationBar'
 
 export default function HomeScreen() {
   return (
     <div>
+      <NavigationBar></NavigationBar>
         <div className="introduction">
             Hello!, <br/>
             If you have not registered, please register on top of the page, <br></br>
